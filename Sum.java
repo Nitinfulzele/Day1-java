19862e58fa6c006a4722dcb9e1a073c03605dbc2
Nitinfulzele <nitinfulzele99@gmail.com>
2022-09-26 09:58:57 +0530

    the sum of command-line arguments

---
+++ Sum.java
@@ -0,0 +1,18 @@
+
+public class Sum {
+
+	public static void main(String[] args) {
+		
+		int n1,n2,n3,sum;
+		
+		n1 = 150;
+		n2 = 200;
+		n3 = 0;
+		
+		sum=n1+n2;
+		System.out.println("SUM =" +sum);
+		System.out.println("Invalid =" +n3);
+
+	}
+
+}
